657a26213d543ea8cd23a2ab3c612d27aaada952
D`Souza, Cedric (Cedric) <dsouzac@avaya.com>
2024-12-07 20:43:05 +0530

    Updated for creating DB and migration script

--- Basic-Postgress/dboperations/src/main/java/com/jpa/postgress/dboperations/controller/UsersController.java
+++ Basic-Postgress/dboperations/src/main/java/com/jpa/postgress/dboperations/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.jpa.postgress.dboperations.controller;
 
 import com.jpa.postgress.dboperations.entity.Users;
 import com.jpa.postgress.dboperations.service.UsersService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +12,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "User Controller", description = "APIs for managing users")
 public class UsersController {
     @Autowired
     private UsersService usersService;
 
     @GetMapping
+    @Tag(name = "User Controller", description = "APIs for managing users")
     public List<Users> getUsers() {
         return usersService.getUsers();
     }
