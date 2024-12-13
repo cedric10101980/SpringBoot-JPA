5941484c89b22b6873f4964d9598ece14bc85567
D`Souza, Cedric (Cedric) <dsouzac@avaya.com>
2024-12-13 09:06:40 +0530

    Updated docker push

--- Basic-MongoDB/createimage.sh
+++ Basic-MongoDB/createimage.sh
@@ -1,4 +1,3 @@
 docker buildx create --use
-docker buildx build --platform linux/amd64 -t cedric10101980/basic-mongodb-app:1.0.0 --push .
 
-#docker buildx build --platform linux/arm64 -t cedric10101980/basic-mongodb-app:1.0.0 --push .
+docker buildx build --platform linux/arm64,linux/amd64 -t cedric10101980/basic-mongodb-app:1.0.0 --push .
